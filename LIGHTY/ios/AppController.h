6e677f51316f690069ef736844a49fc4cf8628ff
kidd0323 <Leung_Shih@shiliangdeMacBook-Air.local>
2014-03-03 20:42:21 +0900

    Initial Commit

---
+++ LIGHTY/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  LIGHTYAppController.h
+//  LIGHTY
+//
+//  Created by TOKI on 14-3-3.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ LIGHTY/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  LIGHTYAppController.h
+//  LIGHTY
+//
+//  Created by TOKI on 14-3-3.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
